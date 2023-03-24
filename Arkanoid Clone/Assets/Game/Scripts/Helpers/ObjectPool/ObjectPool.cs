09619902fb8b95f9f29dc1f48e89f8cd5b5460ae
Ali Hakan Şen <alihakansen4@gmail.com>
2023-03-24 15:31:45 +0300

    chore(ObjectPool): Object pool added.
    Object pool added.

---
+++ Arkanoid Clone/Assets/Game/Scripts/Helpers/ObjectPool/ObjectPool.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private List<GameObject> poolList;
+    public GameObject pooledObject;
+    public int amountOfPool;
+
+    private void Awake()
+    {
+        poolList = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < amountOfPool; i++)
+        {
+            tmp = Instantiate(pooledObject);
+            tmp.SetActive(false);
+            poolList.Add(tmp);
+        }
+    }
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < amountOfPool; i++)
+        {
+            if(!poolList[i].activeInHierarchy)
+            {
+                return poolList[i];
+            }
+        }
+        return null;
+    }
+}
