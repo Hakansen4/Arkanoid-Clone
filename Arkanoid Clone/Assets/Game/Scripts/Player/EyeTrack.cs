15efecf653242caa81ae688201b8e89c8a19af6e
Ali Hakan Şen <alihakansen4@gmail.com>
2023-04-09 18:29:28 +0300

    fix(Eye Track): Eye track Null Controller.
    # Eye track null controller.

--- Arkanoid Clone/Assets/Game/Scripts/Player/EyeTrack.cs
+++ Arkanoid Clone/Assets/Game/Scripts/Player/EyeTrack.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Events.Others;
 using Ambrosia.EventBus;
+using System;
 
 public class EyeTrack : MonoBehaviour
 {
     Transform Ball;
     private void OnEnable()
     {
-        Ball = GameObject.FindGameObjectWithTag("Ball").transform;
+        Ball = GameObject.FindGameObjectWithTag("Ball")?.transform;
     }
+
     private void Update()
     {
         EyeFollow();
     }
     private void EyeFollow()
     {
+        if (Ball == null)
+            return;
+
         Vector2 direction = new Vector2(
             Ball.position.x - transform.position.x,
             Ball.position.y - transform.position.y
